ee80e07c3988900fb5dad7e9ba37b91ec116ac0d
Risto Puolakainen <rpuolak@gmail.com>
2021-08-08 23:23:07 +0300

    Disable polymorph when under kalma's effect

--- files/powers/toggle_kalma.lua
+++ files/powers/toggle_kalma.lua
@@ -1,10 +1,15 @@
 dofile_once("mods/raksa/files/scripts/enums.lua")
+dofile_once("mods/raksa/files/scripts/utilities.lua")
 
 
 function toggle_kalma()
+  local player = get_player()
+
   if GlobalsGetBool(KALMA_IS_IMMORTAL) then
+    EntityRemoveTag(player, "polymorphable_NOT")
     GamePrint("You feel small and frail once again.")
   else
+    EntityAddTag(player, "polymorphable_NOT")
     GamePrint("You're feeling indestructible!")
   end
 
